13c689c0a39075ad2d214057b1cc636f2d376dd0
alepresta <alejandro.modernizacion@gmail.com>
2018-10-09 14:24:35 -0300

    Feat: Turnos Status TERMINADO OK

--- test/cucumber/features/step_definitions/turnos/turnos_status.rb
+++ test/cucumber/features/step_definitions/turnos/turnos_status.rb
@@ -1,26 +1,38 @@
 
 Then /^verificar https turnos-api argentina.gob.ar$/ do
-  # do something
+  pagina ="https://turnos-api.argentina.gob.ar/"
+  go(pagina)
+  status_link(200,pagina)
 end
 
 Then /^https filas-api.argentina.gob.ar$/ do
-  # do something
+  pagina ="https://filas-api.argentina.gob.ar/"
+  go(pagina)
+  status_link(200,pagina)
 end
 
 Then /^https filas.argentina.gob.ar$/ do
-  # do something
+  pagina ="https://filas.argentina.gob.ar"
+  go(pagina)
+  status_link(200,pagina)
 end
 
 Then /^https usuarios-turnos.argentina.gob.ar$/ do
-  # do something
+  pagina ="https://usuarios-turnos.argentina.gob.ar/"
+  go(pagina)
+  status_link(200,pagina)
 end
 
 Then /^http carteleras-filas.argentina.gob.ar$/ do
-  # do something
+  pagina ="http://carteleras-filas.argentina.gob.ar"
+  go(pagina)
+  status_link(200,pagina)
 end
 
 Then /^https usuarios-turnos-api.argentina.gob.ar$/ do
-  # do something
+  pagina ="https://usuarios-turnos-api.argentina.gob.ar"
+  go(pagina)
+  status_link(200,pagina)
 end
 
 
