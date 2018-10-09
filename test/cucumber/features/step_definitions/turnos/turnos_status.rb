87c70d234e99c82b650cefa14f6ba1edba691059
alepresta <alejandro.modernizacion@gmail.com>
2018-10-09 13:29:38 -0300

    Feat: Turnos Status Base

---
+++ test/cucumber/features/step_definitions/turnos/turnos_status.rb
@@ -0,0 +1,27 @@
+
+Then /^verificar https turnos-api argentina.gob.ar$/ do
+  # do something
+end
+
+Then /^https filas-api.argentina.gob.ar$/ do
+  # do something
+end
+
+Then /^https filas.argentina.gob.ar$/ do
+  # do something
+end
+
+Then /^https usuarios-turnos.argentina.gob.ar$/ do
+  # do something
+end
+
+Then /^http carteleras-filas.argentina.gob.ar$/ do
+  # do something
+end
+
+Then /^https usuarios-turnos-api.argentina.gob.ar$/ do
+  # do something
+end
+
+
+
